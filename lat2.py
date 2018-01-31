87f70858a34b8264b287fcfd99bdda67d392cfcd
choiril <choiril@users.noreply.github.com>
2018-01-31 07:59:59 +0700

    Add files via upload
    hari kedua

---
+++ lat2.py
@@ -0,0 +1,10 @@
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+print("I could have code like this.") # and the comment after is ignored
+# You can also use a comment to "disable" or comment out code:
+# print("This won't run.")
+print("This will run.")
+#komentar tidak akan dibaca oleh program
+#komentar pada python di awali dengan tanda pagar (#)
+print("Ini akan ditampilkan dalam program")
+#print("karena ada tanda #, maka tidak dibaca oleh program")
